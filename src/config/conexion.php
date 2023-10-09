811d8f54aa0cb0c20425ec1672aa8dc5d9d7a76d
Oskitar-Ale <oscarvilca66@gmail.com>
2023-10-09 00:15:02 -0500

    conexion establecida a PS

--- src/config/conexion.php
+++ src/config/conexion.php
@@ -1,6 +1,17 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$contrasenia = "123";
-$conexion;
+    require('../../config.php');
+
+
+    $dsn = "mysql:host={$dbHost};dbname={$dbName}";
+    echo var_dump($cSsl);
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => $cSsl,
+    ];
+    try {
+        $pdo = new PDO($dsn, $dbUsername, $dbPassword,$options);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión segura exitosa a la base de datos MySQL.";
+    } catch (PDOException $e) {
+        die("Error de conexión a la base de datos: " . $e->getMessage());
+    }
 ?>
