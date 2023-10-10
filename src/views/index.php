6862d553ddf7f139c762d580e3cad45299944d46
Oskitar-Ale <oscarvilca66@gmail.com>
2023-10-10 03:25:37 -0500

    componentes insertados head-footer-nav-script

---
+++ src/views/index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE HTML>
+<!--
+	TXT by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<?php include('./layouts/head.php'); ?>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<div class="logo container">
+						<div>
+							<h1><a href="index.html" id="logo"></a></h1>
+							<p>VETERINARIA</p>
+						</div>
+					</div>
+				</header>
+
+			<!-- Nav -->
+				<?php include('./layouts/nav.php'); ?>
+
+			<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<h2>BIENVENIDO A ANIMAL LAND</h2>
+						<p>
+							Tenemos como misión ofrecer servicios médicos veterinarios de óptima calidad,<br> contando con amplia experiencia, un excelente trato personalizado y seguimiento de las necesidades de su mascota <br>para prevenir infortunios y alargarles la vida.</p>
+						<a href="#servicios" class="button scrolly">Te ofrecemos</a>
+					</div>
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-12">
+
+								<!-- Highlight -->
+									<section class="box highlight">
+										<ul class="special">
+											<li><a href="#" class="icon solid fa-search"><span class="label">Magnifier</span></a></li>
+											<li><a href="#" class="icon solid fa-tablet-alt"><span class="label">Tablet</span></a></li>
+											<li><a href="#" class="icon solid fa-flask"><span class="label">Flask</span></a></li>
+											<li><a href="#" class="icon solid fa-cog"><span class="label">Cog?</span></a></li>
+										</ul>
+										<header>
+											<h2>A random assortment of icons in circles</h2>
+											<p>And some text that attempts to explain their significance</p>
+										</header>
+										<p>
+											Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. Praesent semper mod quis eget mi. Etiam eu<br />
+											ante risus. Aliquam erat volutpat. Aliquam luctus et mattis lectus amet pulvinar. Nam nec turpis consequat.
+										</p>
+									</section>
+
+							</div>
+							<div class="col-12">
+
+								<!-- Features -->
+									<section class="box features" id="servicios">
+										<h2 class="major"><span>servicios</span></h2>
+										<div>
+											<div class="row">
+												<div class="col-3 col-6-medium col-12-small">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+															<h3><a href="#">Consultas Medicas</a></h3>
+															<p>
+																Contamos con los mejores equipos medicos para tu mascota.
+															</p>
+														</section>
+
+												</div>
+												<div class="col-3 col-6-medium col-12-small">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+															<h3><a href="#">Baños y Cortes</a></h3>
+															<p>
+																Luce a la moda con los mejores baños y cortes para tu mascota.
+															</p>
+														</section>
+
+												</div>
+												<div class="col-3 col-6-medium col-12-small">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+															<h3><a href="#">Hospedaje</a></h3>
+															<p>
+																Un lugar donde tu mascota podra correr y jugar con otros animales.
+															</p>
+														</section>
+
+												</div>
+												<div class="col-3 col-6-medium col-12-small">
+
+													<!-- Feature -->
+														<section class="box feature">
+															<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+															<h3><a href="#">Tienda</a></h3>
+															<p>
+																Los mejores productos de alimentos, medicinas, juguetes y ropa para el engreido del hogar.
+															</p>
+														</section>
+
+												</div>
+												<div class="col-12">
+													<ul class="actions">
+														<li><a href="#" class="button large">Separar</a></li>
+														<li><a href="#" class="button alt large">Horarios</a></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-12">
+
+								<!-- Blog -->
+									<section class="box blog">
+										<h2 class="major"><span>Noticias</span></h2>
+										<div>
+											<div class="row">
+												<div class="col-9 col-12-medium">
+													<div class="content">
+
+														<!-- Featured Post -->
+															<article class="box post">
+																<header>
+																	<h3><a href="#">Las Mejores Campañas para tu engreido de la casa.</a></h3>
+																	<p>With a smaller subtitle that attempts to elaborate</p>
+																	<ul class="meta">
+																		<li class="icon fa-clock">15 minutes ago</li>
+																		<li class="icon fa-comments"><a href="#">8</a></li>
+																	</ul>
+																</header>
+																<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+																<p>
+																	Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in lectus. Praesent
+																	semper mod quis eget mi. Etiam sed ante risus aliquam erat et volutpat. Praesent a
+																	dapibus velit. Curabitur sed nisi nunc, accumsan vestibulum lectus. Lorem ipsum
+																	dolor sit non aliquet sed, tempor et dolor. Praesent a dapibus velit. Curabitur
+																	accumsan.
+																</p>
+																<a href="right-sidebar.html" class="button">Más información</a>
+															</article>
+
+													</div>
+												</div>
+												<div class="col-3 col-12-medium">
+													<div class="sidebar">
+
+														<!-- Archives -->
+															<ul class="divided">
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">A Subheading</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock">6 hours ago</li>
+																			<li class="icon fa-comments"><a href="#">34</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">Another Subheading</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock">9 hours ago</li>
+																			<li class="icon fa-comments"><a href="#">27</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And Another</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock">Yesterday</li>
+																			<li class="icon fa-comments"><a href="#">184</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And Another</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock">2 days ago</li>
+																			<li class="icon fa-comments"><a href="#">286</a></li>
+																		</ul>
+																	</article>
+																</li>
+																<li>
+																	<article class="box post-summary">
+																		<h3><a href="#">And One More</a></h3>
+																		<ul class="meta">
+																			<li class="icon fa-clock">3 days ago</li>
+																			<li class="icon fa-comments"><a href="#">8,086</a></li>
+																		</ul>
+																	</article>
+																</li>
+															</ul>
+															<a href="#" class="button alt">Archives</a>
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</section>
+                <div id="contenido">
+                    <!-- CONTENIDO -->
+                </div>
+			<!-- Footer -->
+			<?php include('./layouts/footer.php'); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include('./layouts/script.php'); ?>
+	</body>
+</html>
